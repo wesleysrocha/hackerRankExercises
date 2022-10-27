a440d6701054170c5501e23c61b994ccfe76232f
wesleysrocha <wesleyrocha.it19@gmail.com>
2022-10-26 22:13:03 -0300

    Diagonal Difference explicacao

---
+++ PlusMinus/PlusMinus/DiagonalDifference.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlusMinus
+{
+    internal class DiagonalDifference
+    {
+        //MATRIZ = [LINHA][COLUNA]
+        //TAMANHO DO ARRAY = 3 .0; 1; 2;
+        //INDICE COMEÇA EM 0 POR ISSO -1 OBTER TAMANHO LINHA
+
+        //esquerdaDireita += arr[i][0 + i];
+
+        //                          3        -0 -1
+        //direitaEsquerda += arr[i][tamanho - i - 1];
+    }
+}
